27bc0a3c2a91bb3604e568c0d093cee2081d162e
Joe Roe <joe@joeroe.io>
2021-02-17 16:29:20 +0100

    Declare namespace of decay constants
    Fixes an R CMD check warning

--- R/units.R
+++ R/units.R
@@ -59,12 +59,12 @@ NULL
 
 #' @rdname c14_units
 #' @export
-c14_age <- function(x, decay = c14_decay_libby) {
+c14_age <- function(x, decay = c14::c14_decay_libby) {
   exp(-decay * x)
 }
 
 #' @rdname c14_units
 #' @export
-c14_f14c <- function(x, decay = c14_decay_libby) {
+c14_f14c <- function(x, decay = c14::c14_decay_libby) {
   -(1 / decay) * log(x)
 }
