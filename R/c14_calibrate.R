joeroe/c14

commit 9ca48c0503a48f014aba961b4513146e1d1e4066

Remove cal metadata functions
Except internal function cal_repair_calibration_range(), which should be repurposed to something used when passing cal objects to rcarbon::spd() etc.

NAMESPACE
@@ -25,7 +25,6 @@ export(c14_f14c)
 export(c14_fcurve)
 export(c14_sum)
 export(cal)
-export(cal_metadata)
 export(cal_point)
 export(read_14c)
 import(vctrs)

R/cal_metadata.R
@@ -1,77 +1,15 @@
 # cal_metadata.R
 # Functions for working with calibration metadata
 
-#' Extract metadata from a calibrated date
+#' Repair calibration range
 #'
-#' @param x  A `cal` object. See [cal()].
+#' rcarbon functions like spd() expect the StartBP and EndBP metadata to be set,
+#' but if the dates came from another source, they might be missing. This function
+#' reconstructs them from the probability distribution.
+#' x should be a cal object.
 #'
-#' @return
-#' A named list of metadata attributes.
-#'
-#' @family cal class methods
-#'
-#' @export
-cal_metadata <- function(x) {
-  attrs <- attributes(x)
-  attrs$names <- NULL
-  attrs$row.names <- NULL
-  attrs$class <- NULL
-
-  return(attrs)
-}
-
-
-# Utility functions (internal) -------------------------------------------
-
-cal_metadata_thesaurus <- function(what = NA) {
-  thesaurus <- tibble::tribble(
-    ~cal,                      ~CalDates,
-    "label",                   "DateID",
-    "c14_age",                 "CRA",
-    "c14_error",               "Error",
-    NA,                        "Details",
-    "curve",                   "CalCurve",
-    "offset",                  "ResOffsets",
-    "offset_error",            "ResErrors",
-    "normalised",              "Normalised",
-    "p_cutoff",                "CalEPS"
-  )
-
-  if(!is.na(what)) {
-    return(thesaurus[[what]])
-  }
-  else {
-    return(thesaurus)
-  }
-}
-
-cal_recode_metadata <- function(x,
-                                from = c("CalDates", "cal"),
-                                to = c("cal", "CalDates")) {
-  from <- match.arg(from)
-  to <- match.arg(to)
-
-  thes <- stats::setNames(cal_metadata_thesaurus(from), cal_metadata_thesaurus(to))
-  thes <- thes[!is.na(names(thes))]
-
-  x <- x[thes]
-  names(x) <- names(thes)
-
-  # Vectors
-  if(to == "CalDates") {
-    x$StartBP <- x$StartBP[1]
-    x$EndBP <- x$EndBP[2]
-  }
-
-  x[sapply(x, is.null)] <- NA
-
-  return(x)
-}
-
-# rcarbon functions like spd() expect the StartBP and EndBP metadata to be set,
-# but if the dates came from another source, they might be missing. This function
-# reconstructs them from the probability distribution.
-# x should be a cal object.
+#' @noRd
+#' @keywords Internal
 cal_repair_calibration_range <- function(x) {
   if(is.null(attr(x, "calibration_range")) ||
      any(is.na(attr(x, "calibration_range")))) {

man/as_cal.Rd
@@ -38,9 +38,6 @@ These functions are intended for complex S3 objects from other packages.
 See \code{\link[=cal]{cal()}} for a more generic constructor, e.g. using a data frame.
 }
 \seealso{
-Other cal class methods: 
-\code{\link{cal_metadata}()}
-
 Other c14 conversion functions: 
 \code{\link{as.CalDates.cal}()}
 }

man/cal_metadata.Rd
@@ -1,22 +1,0 @@
-% Generated by roxygen2: do not edit by hand
-% Please edit documentation in R/cal_metadata.R
-\name{cal_metadata}
-\alias{cal_metadata}
-\title{Extract metadata from a calibrated date}
-\usage{
-cal_metadata(x)
-}
-\arguments{
-\item{x}{A \code{cal} object. See \code{\link[=cal]{cal()}}.}
-}
-\value{
-A named list of metadata attributes.
-}
-\description{
-Extract metadata from a calibrated date
-}
-\seealso{
-Other cal class methods: 
-\code{\link{as_cal}()}
-}
-\concept{cal class methods}

commit d33fac5e5f59605d8bb991ef63db404493dff24d

Remove c14_sum
Also broken in 9ca48c0503a48f014aba961b4513146e1d1e4066. Reimplementation depends on #11. See #12.

NAMESPACE
@@ -26,7 +26,6 @@ export(c14_calibrate)
 export(c14_curve)
 export(c14_f14c)
 export(c14_fcurve)
-export(c14_sum)
 export(cal)
 export(cal_point)
 export(read_14c)

R/c14_calibrate.R
@@ -59,40 +59,6 @@ c14_calibrate <- function(c14_age,
   return(cals)
 }
 
-#' Sum radiocarbon dates with tidy syntax
-#'
-#' A wrapper for [rcarbon::spd()] that takes a list of calibrated dates rather
-#' than a `CalDates` object. This allows you to use the output of [c14_calibrate()] and
-#' take advantage of tidy summary syntax ([dplyr::group_by()], etc.)
-#'
-#' @param cal        A list of `cal` objects.
-#' @param time_range Vector of length 2 indicating the range of calendar dates
-#'                   over which to sum. If left `NA`, the maximum range of the
-#'                   `cal` will be used. See details.
-#' @param ...        Optional arguments to be passed to [rcarbon::spd()]
-#'
-#' @return
-#' A `calGrid` object containing the summed probability distribution.
-#'
-#' @export
-#'
-#' @details
-#' Unlike [rcarbon::spd()], this function will attempt to guess an appropriate
-#' time range if it isn't explicitly specified with `time_range`. It's probably
-#' a good idea to specify it.
-#'
-#' @family tidy radiocarbon functions
-c14_sum <- function(cal, time_range = NA, ...) {
-  cal_dates <- as.CalDates.cal(cal)
-
-  if(is.na(time_range)) {
-    time_range <- c(min.cal(cal), max.cal(cal))
-  }
-
-  summed <- rcarbon::spd(cal_dates, timeRange = time_range, ...)
-  return(list(summed$grid))
-}
-
 #' Generate the normal distribution of a radiocarbon age
 #'
 #' @param age Radiocarbon age(s) in years.

man/c14_calibrate.Rd
@@ -35,8 +35,4 @@ A list of \code{cal} objects.
 Transforms 'raw' radiocarbon ages into a calendar probability distribution
 using a calibration curve.
 }
-\seealso{
-Other tidy radiocarbon functions: 
-\code{\link{c14_sum}()}
-}
 \concept{tidy radiocarbon functions}

man/c14_sum.Rd
@@ -1,35 +1,0 @@
-% Generated by roxygen2: do not edit by hand
-% Please edit documentation in R/c14_calibrate.R
-\name{c14_sum}
-\alias{c14_sum}
-\title{Sum radiocarbon dates with tidy syntax}
-\usage{
-c14_sum(cal, time_range = NA, ...)
-}
-\arguments{
-\item{cal}{A list of \code{cal} objects.}
-
-\item{time_range}{Vector of length 2 indicating the range of calendar dates
-over which to sum. If left \code{NA}, the maximum range of the
-\code{cal} will be used. See details.}
-
-\item{...}{Optional arguments to be passed to \code{\link[rcarbon:spd]{rcarbon::spd()}}}
-}
-\value{
-A \code{calGrid} object containing the summed probability distribution.
-}
-\description{
-A wrapper for \code{\link[rcarbon:spd]{rcarbon::spd()}} that takes a list of calibrated dates rather
-than a \code{CalDates} object. This allows you to use the output of \code{\link[=c14_calibrate]{c14_calibrate()}} and
-take advantage of tidy summary syntax (\code{\link[dplyr:group_by]{dplyr::group_by()}}, etc.)
-}
-\details{
-Unlike \code{\link[rcarbon:spd]{rcarbon::spd()}}, this function will attempt to guess an appropriate
-time range if it isn't explicitly specified with \code{time_range}. It's probably
-a good idea to specify it.
-}
-\seealso{
-Other tidy radiocarbon functions: 
-\code{\link{c14_calibrate}()}
-}
-\concept{tidy radiocarbon functions}