c05996d1d561975ae87f4357c4456f6c7f0f3525
Joe Roe <git@joeroe.io>
2024-10-17 14:18:11 +0200

    Add normalise parameter to cal_sum(), closes #34

--- R/cal_aggregate.R
+++ R/cal_aggregate.R
@@ -10,6 +10,8 @@
 #' @param x A [cal] vector of calendar probability distributions
 #' @param range Range of ages over which to sum. Defaults to the maximum range
 #'   of `x` at one year resolution.
+#' @param normalise Whether to normalise the summed distribution to sum to 1.
+#'   The default is not to normalise.
 #' @param ... Not used
 #'
 #' @return The summed probability distribution as a [cal] vector.
@@ -24,10 +26,13 @@
 #' @examples
 #' shub1_cal <- c14_calibrate(shub1_c14$c14_age, shub1_c14$c14_error)
 #' cal_sum(shub1_cal)
-cal_sum <- function(x, range = cal_age_common(x), ...) {
+cal_sum <- function(x, range = cal_age_common(x), normalise = FALSE, ...) {
   # TODO: ensure x and range have the same era - or is this a job for cal validation?
+  # TODO: normalise interpolated??
   x <- cal_interpolate(x, range)
-  # TODO: normalise?
+
   pdens_sum <- furrr::future_pmap_dbl(cal_pdens(x), \(...) sum(..., na.rm = TRUE))
+  if (isTRUE(normalise)) pdens_sum <- pdens_sum / sum(pdens_sum)
+
   new_cal(list(data.frame(age = cal_age(x)[[1]], pdens = pdens_sum)))
 }
--- tests/testthat/test-cal_aggregate.R
+++ tests/testthat/test-cal_aggregate.R
@@ -3,3 +3,7 @@ x <- c14_calibrate(c(1500, 1600, 1700), rep(20, 3))
 test_that("cal_sum() returns a cal vector of length 1", {
   expect_vector(cal_sum(x), cal(), 1)
 })
+
+test_that("cal_sum(normalise = TRUE) normalised pdens to 1", {
+  expect_equal(sum(cal_pdens(cal_sum(x, normalise = TRUE))[[1]]), 1)
+})
