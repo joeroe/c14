448f8622d85a4ea545aa2d8804590a1dafa6e837
Joe Roe <git@joeroe.io>
2026-07-15 22:42:18 +0200

    Fix plot tests
    So they use a null device and don't leave behind PDF files

--- tests/testthat/test-plot.R
+++ tests/testthat/test-plot.R
@@ -1,48 +1,66 @@
 # Tests for plot.c14_cal
 
 test_that("plot.c14_cal runs without error on single date", {
+  pdf(NULL)
+  on.exit(dev.off())
   x <- cal(1000, 30, IntCal20)
   expect_invisible(plot(x))
 })
 
 test_that("plot.c14_cal runs without error on multiple dates", {
+  pdf(NULL)
+  on.exit(dev.off())
   x <- cal(c(1000, 2000, 3000), c(30, 40, 50), IntCal20)
   expect_invisible(plot(x))
 })
 
 test_that("plot.c14_cal warns and truncates when max.plot exceeded", {
+  pdf(NULL)
+  on.exit(dev.off())
   x <- cal(rep(1000, 25), rep(30, 25), IntCal20)
   expect_warning(plot(x, max.plot = 20), class = "c14_exceeded_max_plot")
 })
 
 test_that("plot.c14_cal allows overriding max.plot", {
+  pdf(NULL)
+  on.exit(dev.off())
   x <- cal(rep(1000, 25), rep(30, 25), IntCal20)
   expect_invisible(plot(x, max.plot = 30))
 })
 
 test_that("plot.c14_cal handles empty cal", {
+  pdf(NULL)
+  on.exit(dev.off())
   x <- cal()
   expect_invisible(plot(x))
 })
 
 test_that("plot.c14_cal accepts custom parameters", {
+  pdf(NULL)
+  on.exit(dev.off())
   x <- cal(1000, 30, IntCal20)
   expect_invisible(plot(x, main = "Test", col = "red", rev_x = FALSE))
 })
 
 test_that("plot.c14_cal accepts resolution parameter", {
+  pdf(NULL)
+  on.exit(dev.off())
   x <- cal(1000, 30, IntCal20)
   expect_invisible(plot(x, resolution = 1))
   expect_invisible(plot(x, resolution = 10))
 })
 
 test_that("plot.c14_cal accepts fixed_xlim parameter", {
+  pdf(NULL)
+  on.exit(dev.off())
   x <- cal(c(1000, 2000, 3000), c(30, 40, 50), IntCal20)
   expect_invisible(plot(x, fixed_xlim = FALSE))
   expect_invisible(plot(x, fixed_xlim = TRUE))
 })
 
 test_that("plot.c14_cal accepts show_curve parameter", {
+  pdf(NULL)
+  on.exit(dev.off())
   x <- cal(c(1000, 2000, 3000), c(30, 40, 50), IntCal20)
   expect_invisible(plot(x, show_curve = FALSE))
   expect_invisible(plot(x, show_curve = TRUE))
